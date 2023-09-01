c0ae258b6397bc10f9db9ccccda093fb7cf653ad
proskillowner <proskillowner@gmail.com>
2023-09-01 02:57:30 +0200

    Update

--- src/Api.js
+++ src/Api.js
@@ -1,5 +1,5 @@
-export const SERVER_URL = 'http://localhost:3010'
-// export const SERVER_URL = 'https://dexther-api.mnconcept.one'
+// export const SERVER_URL = 'http://localhost:3010'
+export const SERVER_URL = 'https://dexther-api.mnconcept.one'
 
 export const API_GET_USER = '/getuser'
 export const API_GET_CONFIG = '/getconfig'
